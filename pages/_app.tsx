03edf3cd0a29ed703b5cb1fbaff077f72de821db
adam-gill <adam.douglas.gill@gmail.com>
2024-01-10 20:58:14 -0500

    added favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,29 @@
-import type { AppProps } from 'next/app'
-import { Toaster } from 'react-hot-toast';
-import { SessionProvider } from 'next-auth/react';
+import type { AppProps } from "next/app";
+import { Toaster } from "react-hot-toast";
+import { SessionProvider } from "next-auth/react";
 
-import Layout from '@/components/Layout'
-import LoginModal from '@/components/modals/LoginModal'
-import RegisterModal from '@/components/modals/RegisterModal'
-import '@/styles/globals.css'
-import EditModal from '@/components/modals/EditModal';
+import Layout from "@/components/Layout";
+import LoginModal from "@/components/modals/LoginModal";
+import RegisterModal from "@/components/modals/RegisterModal";
+import "@/styles/globals.css";
+import EditModal from "@/components/modals/EditModal";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SessionProvider session={pageProps.session}>
-      <Toaster />
-      <RegisterModal />
-      <LoginModal />
-      <EditModal />
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </SessionProvider>
-  )
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.png"/>
+      </Head>
+      <SessionProvider session={pageProps.session}>
+        <Toaster />
+        <RegisterModal />
+        <LoginModal />
+        <EditModal />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </SessionProvider>
+    </>
+  );
 }
